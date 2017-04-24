e57780588e60554671faa34c224b9d49c0be45e7
Garry Hill <garry@magnetised.net>
2017-04-24 16:13:43 +0100

    Update config with updated toolchain URL

--- nerves.exs
+++ nerves.exs
@@ -14,7 +14,7 @@ config app, :nerves_env,
   platform: Nerves.Toolchain.CTNG,
   target_tuple: :arm_unknown_linux_gnueabihf,
   artifact_url: [
-    "https://github.com/nerves-project/toolchains/releases/download/v#{version}/#{app}-#{version}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
+    "https://github.com/strobe-audio/nerves_toolchain_arm_unknown_linux_gnueabihf/releases/download/v#{version}/#{app}-#{version}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
   ],
   checksum: [
     "defconfig",
