166323f31ba00e6c15220aefc9d68d36d095eee0
Garry Hill <garry@magnetised.net>
2017-04-24 16:32:28 +0100

    Fix filename to work with GH release

--- nerves.exs
+++ nerves.exs
@@ -7,6 +7,9 @@ version =
 
 app = :nerves_toolchain_arm_unknown_linux_gnueabihf
 
+# github replaces any '+' in a filename so we proactively replace them with '_'
+version_filename = version |> String.replace("+", "_")
+
 config app, :nerves_env,
   type: :toolchain,
   version: version,
@@ -14,7 +17,7 @@ config app, :nerves_env,
   platform: Nerves.Toolchain.CTNG,
   target_tuple: :arm_unknown_linux_gnueabihf,
   artifact_url: [
-    "https://github.com/strobe-audio/nerves_toolchain_arm_unknown_linux_gnueabihf/releases/download/v#{version}/#{app}-#{version}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
+    "https://github.com/strobe-audio/nerves_toolchain_arm_unknown_linux_gnueabihf/releases/download/v#{version}/#{app}-#{version_filename}.#{Nerves.Env.host_platform}-#{Nerves.Env.host_arch}.tar.xz"
   ],
   checksum: [
     "defconfig",
